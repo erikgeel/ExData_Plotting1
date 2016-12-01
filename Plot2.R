b2b7e360f55922e798bbfd80ae392d75eaca9916
erikgeel <erikgeel@hotmail.com>
2016-12-01 14:11:01 +0100

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,10 @@
+##
+plot1 <- function() {
+  plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+  dev.copy(png, file="plot2.png", width=480, height=480)
+  dev.off()
+  cat("plot2.png has been saved in (Dutch translaton: opslaglocatie)", getwd())
+}
+plot1()
+
+##
